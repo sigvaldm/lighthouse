852c39edf383daea9e727027d382c7ec1a00fb74
sigvaldm <sigvaldm@fys.uio.no>
2019-12-31 11:26:36 +0100

    Version actually used for a long time. Lacks EEPROM.

--- fyr.ino
+++ fyr.ino
@@ -1,6 +1,10 @@
+#include <PciManager.h>
+#include <SoftTimer.h>
 #include <TimerOne.h>
-//#define LIBCALL_ENABLEINTERRUPT
-#include <EnableInterrupt.h>
+#include <Rotary.h>
+#include <Debouncer.h>
+
+// Current LED bulb is 1W. Circuitry supports 2W.
 
 // Pins
 const int ledPin = 9;
@@ -8,87 +12,34 @@ const int encPinA = 2;
 const int encPinB = 3;
 const int buttonPin = 4;
 
-// Maximum intensity used when LED is fully on
-// The numerical range is 0-1023 which corresponds to 0-5V
-volatile int maxBr = 1023;
-volatile float percBr = 1;
-volatile int state = 0;
 
-// Number of (true) intensity levels during ramp-up/down.
-// Due to the eye's square-law detection small increments
-// are needed for low intensities for which reason this
-// number is high.
-const int N=1000;
+volatile int maxBr = 1023;		// Peak brightness [0,1023]. Not linear.
+volatile float percMaxBr = 1;	// Peak brighness linearized [0,1]. Used by incr() and decr().
+volatile int state = 1;			// 0=off, 1=blinking program, 2=on
 
-// Time for ramping up/down brightness
-const int rampTime = 200; // ms
+// Increase/decrease brightness
+void incr();
+void decr();
 
-const unsigned long debounce = 1000; // us
-const unsigned long debounceBtn = 300000; // us
-volatile unsigned long encPinATime;
-volatile unsigned long encPinBTime;
-volatile unsigned long buttonTime;
+// LED actions
+void updateLed(Task *me);				// Frequent update to change PWM according to instantaneous brightness
+void changeLed(Task *me);				// Infrequent update to turn LED on/off according to blinkig program
 
-void decr(){
-	if(percBr>0) percBr -= 0.02;
-	maxBr = 1023*pow(percBr,2);
-}
-
-void incr(){
-	if(percBr<1) percBr += 0.02;
-	maxBr = 1023*pow(percBr,2);
-}
-
-void doButton();
-
-void doEncoderA(){
-
-	if(micros()-encPinATime>debounce){
-
-		if (digitalRead(encPinA) == HIGH){ 
-			if (digitalRead(encPinB) == LOW){  
-				incr();
-			} else {
-				decr();
-			}
-		} else { 
-			if (digitalRead(encPinB) == HIGH){
-				incr();
-			} else {
-				decr();
-			}
-		}
-		Serial.println(maxBr);
-		
-	}
-	
-	encPinATime=micros();
-}
-
-void doEncoderB(){
-	
-	if(micros()-encPinATime>debounce){
+const int N=1000;						// Number of steps during ramp up/down
+volatile int m=0;						// Current perceived intensity level
+volatile int increment = 1;				// Perceived intensity increment of -1, 0 or +1 per update
+const int T = 350;						// [ms] duration of LED ramp up/down
+const long d = T/(0.001*(N+1));			// [us] time between updates
+volatile int loopTime = 0;				// Time in LED's program's loop
+Task taskUpdateLed(0, updateLed);		// Update analog output. Library handles microsecond but constructor only milliseconds. See setup.
+Task taskChangeLed(1, changeLed);		// Change what led is doing according to whic ms we're in.
 
 
-		if(digitalRead(encPinB) == HIGH){   
-			if(digitalRead(encPinA) == HIGH){  
-				incr();
-			} else {
-				decr();
-			}
-		} else { 
-			if(digitalRead(encPinA) == LOW){   
-				incr();
-			} else {
-				decr();
-			}
-		}
-		Serial.println(maxBr);
-	}
-
-	encPinATime=micros();
-	
-} 
+void onRotate(short direction, Rotary* rotary);
+void onButtonPress();
+void onButtonRelease(unsigned long pressTime);
+Rotary rotaryEncoder(encPinA, encPinB, onRotate, true);
+Debouncer button(buttonPin, MODE_CLOSE_ON_PUSH, onButtonPress, onButtonRelease, true);
 
 void setup(){
 	
@@ -98,67 +49,93 @@ void setup(){
 	pinMode(encPinA, INPUT_PULLUP); 
 	pinMode(encPinB, INPUT_PULLUP);
 	pinMode(buttonPin, INPUT_PULLUP);
-	enableInterrupt(encPinA, doEncoderA, CHANGE);
-	enableInterrupt(encPinB, doEncoderB, CHANGE);  
-	enableInterrupt(buttonPin, doButton, FALLING);
 	
-	Serial.begin(9600);
+	PciManager.registerListener(buttonPin, &button);
+	
+	taskUpdateLed.periodMicros=d;	// Hard-set period to get microsecond resolution
+	SoftTimer.add(&taskUpdateLed);
+	SoftTimer.add(&taskChangeLed);
 
 }
 
-void ledOn(int T){
-  long d=T/(0.001*(N+1));  // Delay time (us)
-  for(int m=0;m<=N;m++){
-    float timestep = (float) m/N;
-    float perceived = pow(sin(1.5708*timestep),2);
-    float intensity = pow(perceived,2);
-    Timer1.pwm(ledPin, intensity*maxBr);
-    delayMicroseconds(d);
+void updateLed(Task *me){
+	m += increment;
+	if(m>=N || m<=0){
+		increment = 0;
+	}
+	float perceived = pow(sin(1.5708*((float)m/N)),2);
+	float intensity = pow(perceived,2);
+	Timer1.pwm(ledPin, intensity*maxBr);
+}
+
+void changeLed(Task *me){
+	
+	// increment = +1 (-1) will ramp up (down) until max (min) level is reached
+/*	
+	if(loopTime==0)		increment = +1;
+	if(loopTime==900)	increment = -1;
+	if(loopTime==1800)	increment = +1;
+	if(loopTime==2700)	increment = -1;
+	if(loopTime==3600)	increment = +1;
+	if(loopTime==4500)	increment = -1;
+	if(loopTime==5400)	increment = +1;
+	if(loopTime==8800)	increment = -1;
+
+	loopTime++;
+	
+	if(loopTime==10200)	loopTime=0;
+*/
+	
+	if(loopTime==0)		increment = +1;
+	if(loopTime==1500)	increment = -1;
+	if(loopTime==3000)	increment = +1;
+	if(loopTime==4500)	increment = -1;
+	if(loopTime==6000)	increment = +1;
+	if(loopTime==7500)	increment = -1;
+	if(loopTime==9000)	increment = +1;
+	if(loopTime==15000)	increment = -1;
+
+	loopTime++;
+	
+	if(loopTime==18000)	loopTime=0;	
+	
+}
+
+void decr(){
+	if(percMaxBr>0) percMaxBr -= 0.05;
+	maxBr = 1023*pow(percMaxBr,2);
+}
+
+void incr(){
+	if(percMaxBr<1) percMaxBr += 0.05;
+	maxBr = 1023*pow(percMaxBr,2);
+}
+
+void onRotate(short direction, Rotary* rotary) {
+  if(direction == DIRECTION_CW) {
+    decr();
+  }
+  if(direction == DIRECTION_CCW) {
+    incr();
   }
 }
 
-void ledOff(int T){
-  long d=T/(0.001*(N+1));  // Delay time (us)  
-  for(int m=N;m>=0;m--){
-    float timestep = (float) m/N;
-    float perceived = pow(sin(1.5708*timestep),2);
-    float intensity = pow(perceived,2);
-    Timer1.pwm(ledPin, intensity*maxBr);    
-    delayMicroseconds(d);      
-  }
-}
-
-void ledBlink(int onTime, int offTime){
-	ledOn(rampTime);
-	delay(onTime);
-	ledOff(rampTime);
-	delay(offTime);
-}
-
-void loop(){
+void onButtonPress(){
+	state++;
+	if(state>2) state=0;
 	
 	if(state==0){
-		Timer1.pwm(ledPin,0);
-	} else if(state==1) {
-		ledBlink(500,500);
-		ledBlink(500,500);
-		ledBlink(500,500);
-		ledBlink(3000,1000);
+		increment = 0;
+		m=0;
+	} else if(state==1){
+		m=0;
+		loopTime=0;
+		SoftTimer.add(&taskChangeLed);
 	} else {
-		Timer1.pwm(ledPin,maxBr);
+		increment = 0;
+		SoftTimer.remove(&taskChangeLed);
+		m=N;
 	}
 }
-
-
-void doButton(){
-
-	if(micros()-buttonTime>debounceBtn){
-
-		state++;
-		if(state>2) state=0;
-		
-	}
-	
-	buttonTime = micros();
-
+void onButtonRelease(unsigned long pressTime){
 }
